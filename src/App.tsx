2344b764c6c34adc2b6d5f14155edd1c482eea4a
Carlos <henriqueeb@live.com>
2023-11-01 20:44:26 -0300

    adjust styled search mobile and desktop

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import "./App.css";
-import Header from "./Components/Header";
-import TopBar from "./Components/TopBar";
-import Carousel from "./Components/Carousel";
-import Footer from "./Components/Footer";
-import './index.css'
-
-
+import Header from "./components/Header";
+import TopBar from "./components/TopBar";
+import Carousel from "./components/Carousel";
+import Footer from "./components/Footer";
+import "./index.css";
 
 function App() {
   return (
@@ -15,15 +13,27 @@ function App() {
         <Header />
         {/* ////// */}
       </div>
-      <div style={{color: 'black', fontSize: 20, fontFamily: 'Work Sans', fontWeight: '700', wordWrap: 'break-word'}}>Filmes Mais Bem Conceituados</div>
-        <Carousel media='movie' category="top_rated"  />
-        <Carousel media='tv' category="top_rated"  />
-        <Carousel media='movie' category="popular"  />
-        <Carousel media='tv' category="popular"  />
-        <Carousel media='movie' category="upcoming"  />
-        <Carousel media='tv' category="airing_today"  />
-        <Carousel media='person' category="popular"  />
-      <div><Footer /></div>
+      <div
+        style={{
+          color: "black",
+          fontSize: 20,
+          fontFamily: "Work Sans",
+          fontWeight: "700",
+          wordWrap: "break-word",
+        }}
+      >
+        Filmes Mais Bem Conceituados
+      </div>
+      <Carousel media="movie" category="top_rated" />
+      <Carousel media="tv" category="top_rated" />
+      <Carousel media="movie" category="popular" />
+      <Carousel media="tv" category="popular" />
+      <Carousel media="movie" category="upcoming" />
+      <Carousel media="tv" category="airing_today" />
+      <Carousel media="person" category="popular" />
+      <div>
+        <Footer />
+      </div>
     </>
   );
 }

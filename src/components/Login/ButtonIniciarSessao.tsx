3bd1c8b145951bc6410f3f0e01ad97e02ca6996d
marcusmichelin <marcusmichelin@gmail.com>
2023-11-06 18:44:28 -0300

    Merge branch 'main' of https://github.com/MarcusMichelin/compass-video-grupo3 into feature-ModalPlayer

--- src/components/Login/ButtonIniciarSessao.tsx
+++ src/components/Login/ButtonIniciarSessao.tsx
@@ -1,4 +1,17 @@
+import styles from './ButtonIniciarSessao.module.css'
 
+function ButtonIniciarSessao({onClick}){
 
+    return(
 
+        <div>
 
+            <button onClick={onClick} className={styles.botao}>INICIAR SESSÃO COM TMDB</button>
+            
+        </div>
+
+    )
+
+}
+
+export default ButtonIniciarSessao
